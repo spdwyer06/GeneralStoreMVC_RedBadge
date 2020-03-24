aae05044552feba871010fd207bb766c357ab895
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-03-23 21:42:44 -0400

    Added Details functionality

--- GeneralStoreMVC/Controllers/CustomerController.cs
+++ GeneralStoreMVC/Controllers/CustomerController.cs
@@ -103,5 +103,24 @@ namespace GeneralStoreMVC.Controllers
             }
             return View(customer);
         }
+
+        // GET : Details
+        // Customer/Details/{id}
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _dbContext.Customers.Find(id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
     }
 }
