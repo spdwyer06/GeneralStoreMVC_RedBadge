ab6c74077948943f5c100318720ab50bd7203b5b
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-03-23 21:37:19 -0400

    Added Create & Delete functionality

--- GeneralStoreMVC/Controllers/CustomerController.cs
+++ GeneralStoreMVC/Controllers/CustomerController.cs
@@ -19,5 +19,56 @@ namespace GeneralStoreMVC.Controllers
             List<Customer> orderedList = customerList.OrderBy(customer => customer.LastName).ToList();
             return View(orderedList);
         }
+
+        // Get: Customer
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Customers.Add(customer);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET : Delete
+        // Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Customer customer = _dbContext.Customers.Find(id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST : Delete
+        // Customer/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _dbContext.Customers.Find(id);
+
+            _dbContext.Customers.Remove(customer);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ GeneralStoreMVC/Migrations/202003240129558_InitialMigration.cs
@@ -0,0 +1,27 @@
+namespace GeneralStoreMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false),
+                        LastName = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CustomerID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+        }
+    }
+}
